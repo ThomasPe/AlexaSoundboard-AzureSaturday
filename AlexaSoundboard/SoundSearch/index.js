da27fcf1e28b0292a2ad305b80c4e0164c1be6d6
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-21 15:52:27 +0200

    re-init

--- AlexaSoundboard/SoundSearch/index.js
+++
@@ -1,7 +0,0 @@
-const verifier = require('alexa-verifier');
-
-
-module.exports = function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item:', myQueueItem);
-    context.done();
-};
