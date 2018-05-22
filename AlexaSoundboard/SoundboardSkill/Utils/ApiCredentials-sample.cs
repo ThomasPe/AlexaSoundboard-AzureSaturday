67836cc31a4ad4739b927e1ddf7f38ecb8753f70
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-22 13:57:54 +0200

    Add sample class to store unsplash apikey

---
+++ AlexaSoundboard/SoundboardSkill/Utils/ApiCredentials-sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexaSoundboard.Utils
+{
+    //public static class ApiCredentials
+    //{
+    //    public static string UnsplashKey = "";
+    //}
+}
