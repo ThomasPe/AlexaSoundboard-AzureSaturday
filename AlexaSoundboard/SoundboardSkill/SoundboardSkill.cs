32645ea480f3da7009cf8c3578de88c42da16423
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-22 13:58:18 +0200

    Start implementing logic of Alexa Skill

--- AlexaSoundboard/SoundboardSkill/SoundboardSkill.cs
+++ AlexaSoundboard/SoundboardSkill/SoundboardSkill.cs
@@ -1,35 +1,123 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Alexa.NET.Response.Directive;
+using Alexa.NET.Response.Directive.Templates;
+using Alexa.NET.Response.Directive.Templates.Types;
+using AlexaSoundboard.Utils;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Unsplasharp;
 
 namespace AlexaSoundboard
 {
     public static class SoundboardSkill
     {
+        private static UnsplasharpClient _unsplasharpClient;
+
         [FunctionName("SoundboardSkill")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "alexa-soundboard")]
+            HttpRequestMessage req,
+            [Queue("soundsearch")]
+            IAsyncCollector<string> soundSearchQueue,
+            TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            log.Info("Alexa Soundboard - Triggerd");
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+            // create unsplash client to get images
+            _unsplasharpClient = new UnsplasharpClient(ApiCredentials.UnsplashKey);
 
-            if (name == null)
+            // get skill request
+            var skillRequest = await req.Content.ReadAsAsync<SkillRequest>();
+
+            // check for launch request
+            if (skillRequest?.Request is LaunchRequest)
+                return req.CreateResponse(HttpStatusCode.OK, await CreateStaticRequestResponse("welcome", Statics.WelcomeMessage));
+
+            // check for intent request
+            if (skillRequest?.Request is IntentRequest intentRequest)
             {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
+                switch (intentRequest.Intent.Name)
+                {
+                    case Statics.AmazonStopIntent:
+                        return req.CreateResponse(HttpStatusCode.OK, await CreateStaticRequestResponse("stop", Statics.StopMessage));
+                    case Statics.AmazonHelpIntent:
+                        return req.CreateResponse(HttpStatusCode.OK, await CreateStaticRequestResponse("help", Statics.HelpMessage));
+                    case Statics.AmazonCancelIntent:
+                        return req.CreateResponse(HttpStatusCode.OK, await CreateStaticRequestResponse("cancel", Statics.CancelMessage));
+                }
             }
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            return req.CreateResponse(HttpStatusCode.OK, skillRequest?.Request);
+        }
+
+        private static async Task<SkillResponse> CreateStaticRequestResponse(string searchTerm, string message)
+        {
+            // get a welcome picture
+            var pictureUrl = await GetPhotoAsync(searchTerm);
+
+            // return skill response
+            return string.IsNullOrEmpty(pictureUrl) 
+                ? GetSkillResponse(message, false) 
+                : GetSkillResponse(message, false, new StandardCard { Image = new CardImage { LargeImageUrl = pictureUrl, SmallImageUrl = pictureUrl } });
+        }
+
+        private static async Task<string> GetPhotoAsync(string query)
+        {
+            var photo = await _unsplasharpClient.GetRandomPhoto(UnsplasharpClient.Orientation.Squarish, false, query: query);
+
+            return photo.FirstOrDefault() != null 
+                ? photo.First().Urls.Regular 
+                : string.Empty;
+        }
+
+
+        private static SkillResponse GetSkillResponse(string outputSpeech, bool shouldEndSession, StandardCard card = null, bool useSsml = false)
+        {
+            var response = new ResponseBody
+            {
+                ShouldEndSession = shouldEndSession,
+
+            };
+
+            if (card != null)
+            {
+                response.Card = card;
+                response.Directives = new List<IDirective>
+                {
+                    new DisplayRenderTemplateDirective
+                    {
+                        Template = new BodyTemplate1
+                        {
+                            BackgroundImage = new TemplateImage
+                            {
+                                ContentDescription = "Picture",
+                                Sources = new List<ImageSource> {new ImageSource {Url = card.Image.LargeImageUrl}}
+                            },
+                        }
+                    }
+                };
+            }
+
+            if (useSsml)
+                response.OutputSpeech = new SsmlOutputSpeech { Ssml = outputSpeech };
+            else
+                response.OutputSpeech = new PlainTextOutputSpeech { Text = outputSpeech };
+
+            var skillResponse = new SkillResponse
+            {
+                Response = response,
+                Version = "1.0"
+            };
+
+            return skillResponse;
         }
     }
 }
