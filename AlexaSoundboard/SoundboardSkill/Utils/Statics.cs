42a1374ac26b5eda2a58c36d6291ad16372b3f2e
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-22 13:54:27 +0200

    Added statics to store messages and helpful magic strings

---
+++ AlexaSoundboard/SoundboardSkill/Utils/Statics.cs
@@ -0,0 +1,19 @@
+﻿namespace AlexaSoundboard.Utils
+{
+    public static class Statics
+    {
+        // Intents
+        public static string SoundIntent = "SoundIntent";
+        public const string AmazonStopIntent = "AMAZON.StopIntent";
+        public const string AmazonCancelIntent = "AMAZON.CancelIntent";
+        public const string AmazonHelpIntent = "AMAZON.HelpIntent";
+
+        // Messages
+        public const string StopMessage = "Ok. I will hear you later.";
+        public const string CancelMessage = "Sure.";
+        public const string HelpMessage = "You can ask me to play a short soundclip and I will try to do my very best to play it for you. For example: Play Bazinga."; 
+
+        public static string WelcomeMessage ="Welcome to the Alexa Soundboard. You can ask me to play a short soundclip and I will try to do my very best to play it for you.";
+
+    }
+}
