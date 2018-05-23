b89a62b80e590daf1c00da015ea8619d63fd1fd0
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-23 15:19:33 +0200

    update crawler

--- AlexaSoundboard/SoundSearch/index.js
+++ AlexaSoundboard/SoundSearch/index.js
@@ -1,4 +1,16 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item:', myQueueItem);
-    context.done();
+
+    request('https://www.myinstants.com/search/?name=power+rangers+theme', function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(html);
+            var sound = $('.instant .small-button').first().attr('onmousedown').replace("play('/media/sounds/", "").replace("')", "");
+            console.log(sound);
+        }
+        context.done();
+    });
+
 };
