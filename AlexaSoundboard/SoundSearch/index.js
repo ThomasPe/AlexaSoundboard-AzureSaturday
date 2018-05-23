8f213702b4c140092a012bff84327259659ed8cc
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-23 15:33:12 +0200

    update nodejs binding

--- AlexaSoundboard/SoundSearch/index.js
+++ AlexaSoundboard/SoundSearch/index.js
@@ -1,6 +1,6 @@
-var request = require('request');
-var cheerio = require('cheerio');
-
+const request = require('request');
+const cheerio = require('cheerio');
+const soundbaseuri = 'https://www.myinstants.com/media/sounds/';
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item:', myQueueItem);
 
@@ -9,8 +9,19 @@ module.exports = function (context, myQueueItem) {
             var $ = cheerio.load(html);
             var sound = $('.instant .small-button').first().attr('onmousedown').replace("play('/media/sounds/", "").replace("')", "");
             console.log(sound);
+
+            request(soundbaseuri + sound, function (error, response, file) {
+                if (!error && response.statusCode == 200) {
+                    context.binding.myOutputBlob = file;
+                } else {
+                    context.log(error);
+                }
+                context.done();
+            });
+        } else {
+            context.log(error);
+            context.done();
         }
-        context.done();
     });
 
 };
