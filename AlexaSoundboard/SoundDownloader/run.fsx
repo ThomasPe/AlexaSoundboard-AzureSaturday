ffbb739677bdb9603e705a582510c8b9f0646ad5
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-24 01:09:31 +0200

    update f#

--- AlexaSoundboard/SoundDownloader/run.fsx
+++ AlexaSoundboard/SoundDownloader/run.fsx
@@ -1,4 +1,14 @@
 open System.IO
 
+let GetBlob (storageAcc:string) containerName blobName =
+    let connString = ConfigurationManager.AppSettings.[storageAcc]
+    let storageAccount = CloudStorageAccount.Parse(connString)
+    let blobClient = storageAccount.CreateCloudBlobClient()
+    let container = blobClient.GetContainerReference(containerName)
+    container.GetBlockBlobReference(blobName)
+
 let Run(inputMessage: string, log: TraceWriter) =
     log.Info(sprintf "F# Queue trigger function processed: '%s'" inputMessage)
+
+
+    let cloudBlockBlob = GetBlob "alexasoundboard_STORAGE" "sounds-new" "test.mp3" 
