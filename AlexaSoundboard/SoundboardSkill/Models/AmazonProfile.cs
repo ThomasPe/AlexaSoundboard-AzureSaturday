b9da29d816358f33d6fb2ed8b7f47dc2260339c1
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-23 21:55:34 +0200

    Add model class for amazon profile

---
+++ AlexaSoundboard/SoundboardSkill/Models/AmazonProfile.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace AlexaSoundboard.SoundboardSkill.Models
+{
+    public class AmazonProfile
+    {
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; }
+    }
+}
