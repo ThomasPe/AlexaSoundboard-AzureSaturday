ba037e9688a150849aaac4d9a367e78b0c50952f
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-24 13:26:58 +0200

    fix soundsearch output

--- AlexaSoundboard/SoundSearch/index.js
+++ AlexaSoundboard/SoundSearch/index.js
@@ -3,14 +3,15 @@ const cheerio = require('cheerio');
 const soundbaseuri = 'https://www.myinstants.com/media/sounds/';
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item:', myQueueItem);
-
-    request('https://www.myinstants.com/search/?name=power+rangers+theme', function (error, response, html) {
+    var query = myQueueItem.split(' ').join('+');
+    var name = myQueueItem.split(' ').join();
+    request('https://www.myinstants.com/search/?name=' + query, function (error, response, html) {
         if (!error && response.statusCode == 200) {
             context.log("successful download of html");
             var $ = cheerio.load(html);
             var sound = $('.instant .small-button').first().attr('onmousedown').replace("play('/media/sounds/", "").replace("')", "");
             context.log(sound);
-            context.binding.soundUri = soundbaseuri + sound;
+            context.binding.soundUri = soundbaseuri + sound + ';' + name;
             context.done();
         } else {
             context.log("error: " + error);
