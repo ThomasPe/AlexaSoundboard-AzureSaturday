ffbb36907bd2abb1c3f25c005efabcb1a41e987b
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-24 14:30:37 +0200

    debugging

--- AlexaSoundboard/SoundSearch/index.js
+++ AlexaSoundboard/SoundSearch/index.js
@@ -5,10 +5,14 @@ module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item:', myQueueItem);
     var query = myQueueItem.split(' ').join('+');
     var name = myQueueItem.split(' ').join();
+    context.log('query: ' + query);
+    context.log('name: ' + name);
     request('https://www.myinstants.com/search/?name=' + query, function (error, response, html) {
         if (!error && response.statusCode == 200) {
             context.log("successful download of html");
             var $ = cheerio.load(html);
+            var count = $('.instant .small-button').length;
+            context.log('Sounds found: ' + count);
             var sound = $('.instant .small-button').first().attr('onmousedown').replace("play('/media/sounds/", "").replace("')", "");
             context.log(sound);
             context.binding.soundUri = soundbaseuri + sound + ';' + name;
