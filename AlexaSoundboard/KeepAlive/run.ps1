6e73142d11b529c1440a728db159bfe456af8de6
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-25 13:28:42 +0200

    update keep alive script

--- AlexaSoundboard/KeepAlive/run.ps1
+++ AlexaSoundboard/KeepAlive/run.ps1
@@ -1,4 +1,3 @@
 Write-Output "KeepAlive trigger function executed at:$(get-date)";
-$Url = "https://alexasoundboardskill.azurewebsites.net/api/alexa-soundboard";
-$Response = Invoke-WebRequest $Url
+$Response = Invoke-WebRequest https://alexasoundboardskill.azurewebsites.net/api/alexa-soundboard -Method POST
 Write-Output $Response.StatusCode
