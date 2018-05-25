c0e313fbc7c3678311b3cf678514fd4b654e4cbd
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-25 17:00:37 +0200

    update keepalive

--- AlexaSoundboard/KeepAlive/run.ps1
+++ AlexaSoundboard/KeepAlive/run.ps1
@@ -1,3 +1,7 @@
 Write-Output "KeepAlive trigger function executed at:$(get-date)";
 $YourURI = 'https://alexasoundboardskill.azurewebsites.net/api/alexa-soundboard'
-Invoke-WebRequest -Uri $YourURI -Method POST
+Invoke-WebRequest -Uri $YourURI -Method POST -UseBasicParsing
+
+Write-Output "Done triggering KeepAlive";
+
+
